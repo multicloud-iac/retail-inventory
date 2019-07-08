45b7890de3579cfb9bd85ba8821ec97ace2d0546
Robert Peteuil <robert@peteuil.com>
2019-07-08 13:44:27 -0700

    change instance type

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "instance_type" {
   description = "type of EC2 instance to provision."
-  default     = "t3.micro"
+  default     = "t2.micro"
 }
 
 variable "ami_owner" {
